d247d74d1a1773c58f3cc92893aaccbb2d0f2630
Jamie Zimmerman <jamiez@uoregon.edu>
2017-04-11 15:57:58 -0700

    backup of subprocess implementation

--- check.py
+++ check.py
@@ -1,60 +1,98 @@
 #!/Users/jamiezimmerman/anaconda/bin/python
-#Written by Jamie Zimmerman 3/27/2017
+
+#TODO This shebang path MUST be changed before execution
+# 1. bash$ which python
+# 2. copy the pathname to the first line of this file with proper #!
+
+#-----------------------------------------------------------------------#
+
+#Written by Jamie Zimmerman 3/29/2017
+#Backwards compatible for Python 2.6.6 deployment on RHEL server
+
+
+#Usage: ./check2.py -c [bash command] -o [math operator] -a [amount to operate by]
+#Example: ./check2.py -c 'echo SNMP - OK - -98 total power 25 KW' -o '/' -a 10
+
 import argparse
-import operator
-import subprocess
+from operator import mul, truediv
+#import subprocess
+import sys
+import re
 
-#Usage: python3 check.py -c [bash command] -o [math operator] -a [amount to operate by]
-#Example: python3 check.py -c 'echo SNMP - OK - -98 total power 25 KW' -a 10 -o '/'
-
-def check(string, OP, amount):
-	ops = {'*': operator.mul, '/': operator.truediv}
-
-	li = bashrun(string) #get the string result of whatever the command specified
+def check(command, OP, amount):
+	ops = {'*': mul, '/': truediv}
+	res = subprocess.Popen(command.split(' '), stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
+	#res = "SNMP WARNING - UPS batter Voltage *423*"
+	li = re.split("[= ]+", res) #splits by multiple delimiters: =, <space> 
+	destarred = False #boolean keeps track of a word having **
 	for i in range(len(li)):
 		try:
+			if li[i].startswith('*') and li[i].endswith('*'):
+				li[i] = li[i].strip('*')
+				destarred = True
+			
 			new_value = calculate(float(li[i]), ops[OP], amount)
-			li[i] = "{0: .2f}".format(new_value)
+			
+			if destarred:
+				li[i] = "*{0:.1f}*".format(new_value)
+				destarred = False
+			else:
+				li[i] = "{0:.1f}".format(new_value)
 		except ValueError:
 			pass
-	return " ".join(li)
+	# reg ex split function destroys the equals sign critical to the string
+	# equal sign necessary on very last number only if there was a bar in the bash command
+	if '|' in command:
+		i = find(li)
+		sub = " ".join(li[0:i]) #join the first section of the string
+		sub += "=" #include the lost #
+		while i < len(li):
+			sub += li[i] #add the rest of the list items
+			i += 1
+		return sub
+	else:
+		return " ".join(li)
 
 #----------------------------------------------------------#
-#helper function to run command and get result
-def bashrun(cmd):
-	'''
-	input -> string
-	splits the string, runs the bash command, and returns output from command
-	output -> list (of words generated in the result)
-	'''
-	result = subprocess.run(cmd.split(' '), stdout=subprocess.PIPE)
-	retrieved = result.stdout.decode('utf-8').split(' ')
-	return retrieved
+
+#helper function gets the input string - i.e. the result of the bash command piped into this python program
+def get_command():
+	command = sys.stdin.read().strip()
+	return command 
 
 
 #helper function to calculate new value
-def calculate(input, op, output):
+def calculate(inp, op, out):
 	'''
-	input -> float/int
+	inp -> float/int
 	op -> mathematical function from operator library
-	output -> float/int
+	out -> float/int
 	'''
-	return op(input, output)
+	return op(inp, out)
+
+#helper function finds index of last instance of a number in a list
+def find(lis):
+	for j in range(len(lis)-1, -1, -1):
+		try:
+			dummy = float(lis[j])
+			return j
+		except ValueError:
+			pass
+	return None
 
 
 #----------------------------------------------------------#
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='SNMP get string process')
-	parser.add_argument('-c', "--command", type=str, required=True, help='command to run')
-	parser.add_argument('-o', "--operation", type=str, required=True, help='operation to perform, ie % "%" to divide or "*" to multiply')
+	#parser.add_argument('-c', "--command", type=str, required=True, help='command to run')
+	parser.add_argument('-o', "--operation", type=str, required=True, help='operation to perform, ie: "/" to divide or "*" to multiply')
 	parser.add_argument('-a', "--amount", type=int, required=True, help='int(amount) to divide/multiply by')
 	
 	args = parser.parse_args()
 
-	command = args.command
+	#command = args.command
 	operation = args.operation
 	amount = args.amount
 
-	#print("command is {}\noperate is {}\namount is {}".format(command, operation, amount))
 	res = check(command, operation, amount)
 	print(res)
